926c3f62a0baf2aadb144df2bdc4b40f88af03ce
codytseng <codytseng98@gmail.com>
2025-11-19 21:41:20 +0800

    fix: correct mistyped kind

--- src/services/local-storage.service.ts
+++ src/services/local-storage.service.ts
@@ -177,10 +177,14 @@ class LocalStorageService {
         showKindSet.add(ExtendedKind.ADDRESSABLE_NORMAL_VIDEO)
         showKindSet.add(ExtendedKind.ADDRESSABLE_SHORT_VIDEO)
       }
+      if (showKindsVersion < 3 && showKindSet.has(24236)) {
+        showKindSet.delete(24236) // remove typo kind
+        showKindSet.add(ExtendedKind.ADDRESSABLE_SHORT_VIDEO)
+      }
       this.showKinds = Array.from(showKindSet)
     }
     window.localStorage.setItem(StorageKey.SHOW_KINDS, JSON.stringify(this.showKinds))
-    window.localStorage.setItem(StorageKey.SHOW_KINDS_VERSION, '2')
+    window.localStorage.setItem(StorageKey.SHOW_KINDS_VERSION, '3')
 
     this.hideContentMentioningMutedUsers =
       window.localStorage.getItem(StorageKey.HIDE_CONTENT_MENTIONING_MUTED_USERS) === 'true'
