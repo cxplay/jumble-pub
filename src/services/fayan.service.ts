ec03a49e32e19d9b90b6170aeaf893535f3112db
codytseng <codytseng98@gmail.com>
2025-12-28 22:47:16 +0800

    fix: 🐛

--- src/services/fayan.service.ts
+++ src/services/fayan.service.ts
@@ -17,7 +17,7 @@ class FayanService {
           return new Array(pubkeys.length).fill(null)
         }
         const data = await res.json()
-        return pubkeys.map((pubkey) => data[pubkey] ?? 0)
+        return pubkeys.map((pubkey) => data[pubkey]?.percentile ?? 0)
       } catch {
         return new Array(pubkeys.length).fill(null)
       }
